7ab4bb67f541cb0b4a585175b9cf5becdbb38d12
Bhupal <bhupal4all@users.noreply.github.com>
2017-12-19 00:02:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'dist')));
 
 app.get('*', (req,res) => {
-res.sendFile(path.join(__dirname,'dist/index.html'));
+res.sendFile(path.join(__dirname,'index.html'));
 
 const port = process.env.PORT || '3001';
 app.set('port', port);
